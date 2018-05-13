0c251ace0c91ab246c3cd1be4093359a2f5a59c5
Thái Minh Sang <sang024854@gmail.com>
2018-05-14 00:54:56 +0700

    enable cors

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ var app = express();
 // Mongoose
 
 const mongoDB =
-	"mongodb://admin:admin@ds117590.mlab.com:17590/shopping-website-spa";
+"mongodb://admin:admin@ds117590.mlab.com:17590/shopping-website-spa";
 mongoose.Promise = global.Promise;
 mongoose
-	.connect(mongoDB)
-	.then(() => {
-		console.log("connection success");
-	})
-	.catch(error => {
-		console.log("Connect fail: ", error.stack);
-		process.exit(1);
-	});
+.connect(mongoDB)
+.then(() => {
+	console.log("connection success");
+})
+.catch(error => {
+	console.log("Connect fail: ", error.stack);
+	process.exit(1);
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -39,6 +39,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// enable cors
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+		);
+	res.header("Access-Control-Allow-Methods", "*");
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 app.use("/", indexRouter);
 
