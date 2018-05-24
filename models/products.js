7c44051753a5b85d6e71b358dc247a7deb38a775
Trần Tuấn Sang <tuansang9710@gmail.com>
2018-05-24 20:12:06 +0700

    Add method lookup
    Add method lookup
    Update model products

--- models/products.js
+++ models/products.js
@@ -2,16 +2,47 @@ var mongoose = require("mongoose");
 
 var schema = mongoose.Schema({
 	ProName: String,
-	TinyDes: String,
 	FullDes: String,
 	Prize: Number,
 	CatID: Number,
-	IDNSX: Number,
+	IDProducer: Number,
 	Quantity: Number,
-	SLB: Number,
-	LX: Number,
-	XX: String,
-	SLAnh: String
+	SellQuantity: Number,
+	View: Number,
 });
+
+dbo
+	.collection("products")
+	.aggregate([
+		{
+			$lookup: {
+				from: "categories",
+				localField: "CatID",
+				foreignField: "CatID",
+				as: "categoriesDetail",
+			},
+		},
+	])
+	.toArray(function(err, res) {
+		if (err) throw err;
+		console.log(JSON.stringify(res));
+	});
+
+dbo
+	.collection("products")
+	.aggregate([
+		{
+			$lookup: {
+				from: "nsx",
+				localField: "IDProducer",
+				foreignField: "IDProducer",
+				as: "producerDetail",
+			},
+		},
+	])
+	.toArray(function(err, res) {
+		if (err) throw err;
+		console.log(JSON.stringify(res));
+	});
 
 module.exports = mongoose.model("products", schema, "products");
