e73b855ef856310dabf6702f65f498f36468e7d2
Thái Minh Sang <sang024854@gmail.com>
2018-06-17 00:27:31 +0700

    add route for user
    - change detail, password
    - find orders of id

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
-var userModel = require("../models/users");
+const userModel = require("../models/users");
+const orderModel = require("../models/orders");
 
 exports.create = async (req, res) => {
 	let [err, user] = await to(
@@ -124,5 +125,103 @@ exports.delete = function(req, res) {
 			return res.status(500).send({
 				message: `Error when delete with id ${req.params.id}`,
 			});
+		});
+};
+
+exports.updateDetails = async (req, res) => {
+	userModel
+		.findByIdAndUpdate(
+			req.params.id,
+			{
+				Name: req.body.Name,
+				Email: req.body.Email,
+				DOB: req.body.DOB,
+			},
+			{ new: true },
+		)
+		.then(doc => {
+			if (!doc) {
+				return res.status(404).send({
+					message: `Not found with id ${req.params.id}`,
+				});
+			}
+
+			return res.send(doc);
+		})
+		.catch(err => {
+			if (err.kind === "ObjectId") {
+				return res.status(404).send({
+					message: `Not found with id ${req.params.id}`,
+				});
+			}
+			return res.status(500).send({
+				message: "Error when finding!",
+			});
+		});
+};
+
+exports.updatePassword = async (req, res) => {
+	const OldPass = req.body.OldPass,
+		NewPass = req.body.NewPass,
+		ConNewPass = req.body.ConNewPass;
+
+	// Compare new Password
+	if (NewPass !== ConNewPass) {
+		return res.status(404).send({
+			message: `New Pass must match Confirm Pass`,
+		});
+	}
+
+	// Check old Password
+	let [err, user] = await to(userModel.findById(req.params.id));
+	const [, result] = await to(user.comparePassword(OldPass));
+
+	if (!!!result) {
+		return res.status(404).send({
+			message: `Old Password is wrong`,
+		});
+	}
+
+	// Hash new Password
+	[, user.Password] = await to(user.hashPassword(NewPass));
+
+	userModel
+		.findByIdAndUpdate(
+			req.params.id,
+			{
+				Password: user.Password,
+			},
+			{ new: true },
+		)
+		.then(doc => {
+			if (!doc) {
+				return res.status(404).send({
+					message: `Not found with id ${req.params.id}`,
+				});
+			}
+
+			return res.send(doc);
+		})
+		.catch(err => {
+			if (err.kind === "ObjectId") {
+				return res.status(404).send({
+					message: `Not found with id ${req.params.id}`,
+				});
+			}
+			return res.status(500).send({
+				message: "Error when finding!",
+			});
+		});
+};
+
+exports.findOrders = function(req, res) {
+	orderModel
+		.find({ User_Detail: req.params.id })
+		.then(docs => {
+			res.status(200).send(docs);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).send({ message: "Error when finding" });
 		});
 };
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const users = require("../controllers/users");
 
 router.post("/", users.create);
+
 // Retrieve all Note
 router.get("/", users.findAll);
+
 // Retrieve a single Note with noteId
 router.get("/:id", users.findOne);
+router.get("/:id/orders", users.findOrders);
+
 // Update a Note with Id
 router.put("/:id", users.update);
+router.put("/:id/details", users.updateDetails);
+router.put("/:id/password", users.updatePassword);
+
 // Delete a Note with Id
 router.delete("/:id", users.delete);
 
